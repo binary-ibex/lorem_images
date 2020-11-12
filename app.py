0dbaaa8f16685a031ddd384c286fc3c6dc7ed924
binary-ibex <70746835+binary-ibex@users.noreply.github.com>
2020-11-12 22:45:54 +0530

    add regex and image selection logic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_file
 from PIL import Image
 import io 
+import random 
+import re
 
 app = Flask(__name__)
 
@@ -8,10 +10,46 @@ app = Flask(__name__)
 @app.route('/<int:width>')
 @app.route('/<int:width>/<int:height>')
 def index(width=0, height=0):
+
+    expression = r'\b\d{1,4}\b'
+    width = int(width)
+    height = int(height)
+
+    if not re.findall(expression , str(width)) and re.findall(expression , str(height)):
+        return "Error in input"
+    if width > 2000 or height > 2000:
+        return "image dimension should not exced 2000px"
+        
+    if width == height==0:
+        width = 100
+        height = 100
+    elif width == 0:
+        width = height
+    elif height == 0:
+        height = width
+
     return get_image(width, height)
 
+
+def get_correct_image(width, height):
+    images = {
+        'square':['square1.jpg', 'square2.jpg', 'square3.jpg', 'square4.jpg'],
+        'portrait':['portrait1.jpg', 'portrait2.jpg', 'portrait3.jpg'],
+        'landscape':['landscape1.jpg', 'landscape2.jpg', 'landscape3.jpg']
+    }
+
+    if width == height:
+        return 'images/square/' + random.choice(images['square'])
+    
+    if width>height:
+        return 'images/landscape/' + random.choice(images['landscape'])
+    
+    if width<height:
+        return 'images/portrait/' + random.choice(images['portrait'])
+
+
 def get_image(width, height):
-    image  = Image.open('images/image1.jpg')
+    image  = Image.open(get_correct_image(width, height))
     new_image = image.resize((width, height))
 
     #temp memory buffer 
