62b7796d4be360fdc4df350c107a8eee59dcec68
binary-ibex <70746835+binary-ibex@users.noreply.github.com>
2020-11-12 20:56:10 +0530

    add get_image function and test it

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask 
-
+from flask import Flask, send_file
+from PIL import Image
+import io 
 
 app = Flask(__name__)
 
@@ -7,9 +8,18 @@ app = Flask(__name__)
 @app.route('/<int:width>')
 @app.route('/<int:width>/<int:height>')
 def index(width=0, height=0):
-    return "width = {0} and height = {1}".format(width, height)
+    return get_image(width, height)
 
+def get_image(width, height):
+    image  = Image.open('images/image1.jpg')
+    new_image = image.resize((width, height))
 
+    #temp memory buffer 
+    image_mem = io.BytesIO()
+    new_im = new_image.save(image_mem, 'JPEG')
+    image_mem.seek(0,0)
+
+    return send_file(image_mem,mimetype='image/gif', as_attachment=False)
 
 if __name__=='__main__':
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
